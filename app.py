b8bd69cef9d885b33d1f2d73acca3cb015e68baa
spokkal <spokkal@ncsu.edu>
2014-11-05 17:40:29 -0500

    mongodb integration

--- app.py
+++ app.py
@@ -1,52 +1,55 @@
 import web
-from session import MongoStore
+from text_processing import*
 import pymongo
 import datetime
-
 
 from pymongo import Connection
 connection = Connection()
 
 db = connection['CookWithMe']
 conversation = db['conversation']
-db = c.webpy
 
-#Create a new session object, passing the db and collection name to the MongoStore object
-session = web.session.Session(app, MongoStore(db, 'sessions'))
+web.config.debug = False
 
-#If you want to do user authentication and stuff aswell, add these two lines
-users.session = session
-users.collection = db.users
-users.SALTY_GOODNESS = u'RANDOM_SALT'
-
-
-def make_text(string):
-	dialog = {"date": datetime.datetime.utcnow(), 
-			  "speech": string}
-	conversation.insert(dialog);
-	connection.close();
-	print string;
-	return string.upper();
+processing = Text_Processing()
 
 urls = ('/', 'tutorial')
 render = web.template.render('templates/')
 
 app = web.application(urls, globals())
 
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'context': None})
+        
+
 my_form = web.form.Form(
                 web.form.Textbox('', class_='textfield', id='textfield'),
                 )
-
+	
+def make_text(string, context):
+	toReturn = ""
+	
+	print(string)
+	if "CHEF" in string.upper(): toReturn = processing.process(string.upper(), context)
+	
+	dialog = {"date": datetime.datetime.utcnow(), 
+			  "user": string, "Chef": toReturn}
+	conversation.insert(dialog);
+	connection.close();
+	
+	return toReturn
+	
 class tutorial:
+		
     def GET(self):
+    	session.context = Initial_State()
         form = my_form()
-        return render.tutorial(form, "Your text goes here.")
+    	return render.tutorial(form, "Your text goes here.")
         
     def POST(self):
         form = my_form()
         form.validates()
         s = form.value['textfield']
-        return make_text(s)
+        return make_text(s, session.context)
 
 if __name__ == '__main__':
     app.run()
